78e2c1637222cb3ddb98f73cf45602ccdf8fc2a5
LoganLieou <logan.lieou@gmail.com>
2021-12-02 15:31:19 -0600

    C++ solutions

--- src/day2/day2.cpp
+++ src/day2/day2.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int solutions(string filename) {
+int part1(string filename) {
 	ifstream file;
 	file.open(filename);
 
@@ -26,6 +26,31 @@ int solutions(string filename) {
 	return horizontal * depth;
 }
 
+int part2(string filename) {
+	ifstream file;
+	file.open(filename);
+
+	string direction = "";
+	int pos = 0, dep = 0, aim = 0, value = 0;
+
+	// parse file and do operations
+	if (file.is_open()) {
+		while (file >> direction >> value) {
+			if (direction == "forward") {
+				pos += value;
+				dep += aim * value;
+			} else if (direction == "up") {
+				aim -= value;
+			} else if (direction == "down") {
+				aim += value;
+			}
+		}
+	}
+
+	return pos * dep;
+}
+
 int main() {
-	cout << solutions("input.txt") << endl;
+	cout << part1("input.txt") << endl;
+	cout << part2("input.txt") << endl;
 }
---
+++ src/day2/day2.jl
@@ -0,0 +1,42 @@
+parse_line(x) = let fields = split(x, " ")
+	Symbol(fields[1]), parse(Int, fields[2])
+end
+f = open("input.txt", "r"); lines = parse_line.(readlines(f)); close(f)
+
+function part1(input)
+	pos = 0
+	dep = 0
+	for (cmd, x) in input
+		if cmd == :forward
+			pos += x
+		elseif cmd == :up
+			dep -= x
+		elseif cmd == :down
+			dep += x
+		else
+			error("bad input")
+		end
+	end
+	return pos * dep
+end
+
+@show part1(lines)
+
+function part2(input)
+	pos = dep = aim = 0
+	for (cmd, x) in input
+		if cmd == :forward
+			pos += x
+			dep += aim * x
+		elseif cmd == :up
+			aim -= x
+		elseif cmd == :down
+			aim += x
+		else
+			error("bad input!")
+		end
+	end
+	return pos * dep
+end
+
+@show part2(lines)
